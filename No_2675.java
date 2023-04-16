84fdee443689e9cad25166e126ca250d7a11db35
GyeongMin <aklmo3778@naver.com>
2023-04-16 15:40:00 +0900

    백준 20546번 ( BufferedReader 사용)

---
+++ No_2675.java
@@ -0,0 +1,31 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class No_2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        System.out.println(t);
+
+        String[] str = null;
+        for(int i = 0; i < t; i++){
+            str = br.readLine().split(" ");
+            int r = Integer.parseInt(str[0]);
+
+            for(byte b : str[1].getBytes()){
+                for(int k = 0; k < r; k++){
+                    System.out.print((char) b);
+                }
+            }
+            System.out.println();
+        }
+        br.close();
+
+    }
+}
