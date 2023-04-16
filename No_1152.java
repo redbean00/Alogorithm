9dbbb9a335054e382a2222431f71f47e30664f35
GyeongMin <aklmo3778@naver.com>
2023-04-16 16:27:52 +0900

    백준 1152번 -StringTokenizer 사용

--- No_1152.java
+++ No_1152.java
@@ -1,22 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class No_1152 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] str = br.readLine().split(" ");
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        System.out.println(st.countTokens());
 
-        int count = str.length;
-
-        for(int i = 0; i < str.length; i++){
-            if(str[i].equals("")){
-                count--;
-            }
-        }
-
-        System.out.println(count);
+        br.close();
 
     }
 }
