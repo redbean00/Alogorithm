50953000c980e55571f05c0ccc822cb529b550a2
GyeongMin <aklmo3778@naver.com>
2023-04-16 17:00:24 +0900

    백준 2098 -StringBuilder 사용

---
+++ No_2908.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class No_2908 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        String a = new StringBuilder(sc.next()).reverse().toString();
+        String b = new StringBuilder(sc.next()).reverse().toString();
+
+        System.out.println(Integer.parseInt(a) > Integer.parseInt(b) ? a : b);
+
+        sc.close();
+    }
+}
