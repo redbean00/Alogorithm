3504f8874c183a0c319c75edb71041d71d85f764
GyeongMin <aklmo3778@naver.com>
2023-04-16 15:51:43 +0900

    백준 20546번 ( BufferedReader 사용)

--- No_2675.java
+++ No_2675.java
@@ -1,10 +1,6 @@
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-
-import static java.lang.Integer.parseInt;
 
 public class No_2675 {
     public static void main(String[] args) throws IOException {
